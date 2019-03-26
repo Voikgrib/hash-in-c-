105894736e5ba9b0f1d91f8466a05cfff168bd9d
Vladimir <32133129+Voikgrib@users.noreply.github.com>
2019-03-26 21:44:57 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,22 @@
+
+#include"hash.hpp"
+
+int main()
+{
+	c_hashTable test(10);
+	test.upload("words.txt");
+	char str[100] = {0};
+
+	scanf("%s", str);
+
+	if(test.cmp(str) == true)
+		printf("Yep!\n");
+	else
+		printf("Nope!\n");
+
+
+	return 0;
+}
+
+
+
